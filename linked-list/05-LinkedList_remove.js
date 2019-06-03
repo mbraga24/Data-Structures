1cc74d965b6ca446b681fcd6f257c2a85ff65156
Marlon Braga <rdg.brg@gmail.com>
2019-06-02 22:28:01 -0400

    05-LinkedList_remove.js

---
+++ linked-list/05-LinkedList_remove.js
@@ -0,0 +1,56 @@
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  let newNode = new Node(value, this.head, null);
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+  this.head = newNode;
+}
+
+LinkedList.prototype.addToTail = function(value) {
+  let newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+}
+
+LinkedList.prototype.removeHead = function () {
+  if (!this.head) return null;
+
+  let value = this.head.value;
+  this.head = this.head.next;
+  if (this.head) {
+    this.head.prev = null;
+  } else {
+    this.tail = null;
+  }
+  return value;
+}
+
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) return null;
+
+  let value = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) {
+    this.tail.next = null;
+  } else {
+    this.head = null;
+  }
+  return value;
+}
