43f16fcd5002ea3e7d84c8cb707969270b1f4b20
Marlon Braga <rdg.brg@gmail.com>
2019-06-02 22:29:55 -0400

    Delete file / Change names

--- linked-list/03-LinkedList_add.js
+++ linked-list/03-LinkedList_add.js
@@ -19,9 +19,20 @@ LinkedList.prototype.addToHead = function(value) {
   this.head = newNode;
 }
 
+LinkedList.prototype.addToTail = function(value) {
+  let newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+}
+
 let LinkedL = new LinkedList();
 
 LinkedL.addToHead(100);
 LinkedL.addToHead(200);
+LinkedL.addToTail(333);
 
 console.log(LinkedL);
