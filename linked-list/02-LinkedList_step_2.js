51c7e2444f8755be0e7523fdf34e17cd1440c2e6
Marlon Braga <rdg.brg@gmail.com>
2019-06-02 22:12:50 -0400

    Delete file / Change names

--- linked-list/02-LinkedList_step_2.js
+++ linked-list/02-LinkedList_step_2.js
@@ -9,14 +9,15 @@ function Node(value, next, prev) {
   this.prev = prev;
 }
 LinkedList.prototype.addToHead = function(value) {
-
-  // 'For the 'next' parameter (next node) we will pass the 'this.head' which will refer to the current head or old head
-  // of the linked list. The reason why we add 'this.head' is that if we add the new node to the beginnig of the linked
-  // list the following node will be the old head or what it is now the 'this.head'.'
-
-  // "For the 'prev' parameter we pass 'null' because there will be no nodes coming before the new node"
-  
   let newNode = new Node(value, this.head, null);
-
-
+  // "If the Linked List has already nodes in it or if it already has a head set that head's previous pointer equal to our 'newNode'"
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+  // If the Linked List is empty set the tail node to the newNode because the newNode will be the only one in the list, therefore
+  // it will be both the head node and the tail node.
+    this.tail = newNode;
+  }
+  // Whether the Linked List was previously empty or not we will assign the new node to be the head of the list.
+  this.head = newNode;
 }
---
+++ linked-list/04-LinkedList_addToTail.js
@@ -0,0 +1,32 @@
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  let newNode = new Node(value, this.head, null);
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+  this.head = newNode;
+}
+
+LinkedList.prototype.addToTail = function(value) {
+  let newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+}
+
+let linkedL = new LinkedList();
