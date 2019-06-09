1cf609a21059f16741abbec99326ebd18763ef82
Marlon Braga <rdg.brg@gmail.com>
2019-06-09 15:08:04 -0400

    binary_search_tree/01-BST_recursion and 02-BST-insert

---
+++ binary_search_tree/02-BST-insert.js
@@ -0,0 +1,23 @@
+// Constructor Function 
+function BST(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+// Adding method to BST prototype
+BST.prototype.insert = function(value) {
+  if (value <= this.value) {
+    if (!this.left) {
+      this.left = new BST(value); 
+    } else if (this.left) {
+      this.left.insert(value)
+    }
+  } else if (value > this.value) {
+    if (!this.right) {
+      this.right = new BST(value)
+    } else if (this.right) {
+      this.right.insert(value);
+    }
+  }
+}
