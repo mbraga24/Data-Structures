f5f99e3dacb1d9f03b2ed460d7fc6f2b6be1c0bd
Marlon Braga <rdg.brg@gmail.com>
2019-07-24 16:34:59 -0400

    Hash Table function prototype

---
+++ Hash Table/01-HT_constructor_Func.js
@@ -0,0 +1,14 @@
+function HashTable(size) {
+  this.buckets = Array(size);
+  this.numBuckets = this.buckets.length;
+}
+
+function HashNode(key, value, next) {
+ this.key = key;
+ this.value = value;
+ this.next = next || null;
+}
+
+let myHT = new HashTable(30);
+
+console.log(myHT)
